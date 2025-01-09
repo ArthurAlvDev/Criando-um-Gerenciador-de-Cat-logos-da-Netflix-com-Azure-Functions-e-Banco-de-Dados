c38029be1ca7c76bf8ce10abaa84a797dcc89311
ArthurAlvDev <69433870+ArthurAlvDev@users.noreply.github.com>
2025-01-08 21:43:09 -0300

    Update Código para adicionar um filme.py

--- Código para adicionar um filme.py
+++ Código para adicionar um filme.py
@@ -6,7 +6,7 @@ from azure.cosmos import CosmosClient
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    url = "https://conta>/"
+    url = "conta"
     key = "chave>"
 
     client = CosmosClient(url, credential=key)
